9bc855c6698a5a833b26c1ed579e76de209067c1
Paco Chacon <pacochacon94@gmail.com>
2017-10-14 16:29:33 -0500

    Use authentication in dependences controller

--- app/controllers/api/v1/dependences_controller.rb
+++ app/controllers/api/v1/dependences_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::DependencesController < ApplicationController
+	before_action :authenticate_user!
 	def index
 		@dependences = Dependence.all
-		render json: {dependences: @dependences}
+		render json: {dependences: @dependences}, status: 200
 	end
 end
