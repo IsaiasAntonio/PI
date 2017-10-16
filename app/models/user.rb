59d7e4c47d636e6e1a8627c0e04f8817fcfe200f
Paco Chacon <pacochacon94@gmail.com>
2017-10-16 01:56:53 -0500

    Added user relation to dependences and user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
+  belongs_to :dependence
+  belongs_to :user_role
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  def as_json(options={})
+		super(:include => [:dependence, :user_role])
+  end
 end
