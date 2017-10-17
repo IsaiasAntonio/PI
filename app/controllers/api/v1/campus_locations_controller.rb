e059f98726bc730ad04f8847782074388f58eaf1
Paco Chacon <pacochacon94@gmail.com>
2017-10-16 23:16:34 -0500

    Fixed sintax errors

--- app/controllers/api/v1/campus_locations_controller.rb
+++ app/controllers/api/v1/campus_locations_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CampusLocationsController < ApplicationController
 	def index
 		@campus = CampusLocation.all
-		render json: {campus_locations: @campus}
+		render json: { campus_locations: @campus }, status: 200
 	end
 end
