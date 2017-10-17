1e03f41bee407af6a6a06a25f486625e3bcb7c11
Paco Chacon <pacochacon94@gmail.com>
2017-10-16 22:55:32 -0500

    Added index for ticket states

--- app/controllers/api/v1/ticket_states_controller.rb
+++ app/controllers/api/v1/ticket_states_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::TicketStatesController < ApplicationController
+	def index
+		@ticket_states = TicketState.all
+		render json: { ticket_states: @ticket_states }, status: 200
+	end
 end
