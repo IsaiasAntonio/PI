934d3128f17c4e8f33dd7874b66a424eae4a16f3
Paco Chacon <pacochacon94@gmail.com>
2017-10-22 01:42:13 -0500

    Added private methods for dependences

--- app/controllers/api/v1/dependences_controller.rb
+++ app/controllers/api/v1/dependences_controller.rb
@@ -1,7 +1,19 @@
 class Api::V1::DependencesController < ApplicationController
-	before_action :authenticate_user!
 	def index
 		@dependences = Dependence.all
 		render json: { dependences: @dependences }, status: 200
+	end
+
+	private
+	def dependence_params
+		params.require(:dependence).permit(:name, :campus_location_id)
+	end
+
+	def get_dependence
+		@dependence ||= Dependence.find_by(id: params[:id])
+	end
+
+	def render_record_not_found
+		render json: { error: "La dependencia no se encontró." }, status: 404
 	end
 end
