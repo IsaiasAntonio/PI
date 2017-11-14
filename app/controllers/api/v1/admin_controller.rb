6c3390544cd31ad244f607736a51316957fb3809
Paco Chacon <pacochacon94@gmail.com>
2017-11-14 09:44:35 -0600

    Add Admins documentation

--- app/controllers/api/v1/admin_controller.rb
+++ app/controllers/api/v1/admin_controller.rb
@@ -1,7 +1,11 @@
+# Es una clase que al igual que devise, usa el modelo de User como base.
 class Api::V1::AdminController < ApplicationController
+	# Si ActiveRecord regresa un error porque no encontró un registro en específico va a mandar llamar la función de render_record_not_found
 	rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+	# Antes de que se ejecute cualquier método se pide que el usuario esté loggeado.
 	before_action :authenticate_user!
 
+	# Regresa todos los usuarios si es que el usuario es administrador.
 	def index
 		if current_user.user_role_id == 1
 			@users = User.all
@@ -11,6 +15,7 @@ class Api::V1::AdminController < ApplicationController
 		end
 	end
 
+	# Muestra los detalles de cualquier usuario si es que el usuario es administrador.
 	def show
 		if current_user.user_role_id == 1
 			get_user
@@ -20,6 +25,7 @@ class Api::V1::AdminController < ApplicationController
 		end
 	end
 
+	# Si es que el usuario es administrador, permite actualizar los datos de cualquier usuario.
 	def update
 		if current_user.user_role_id == 1
 			get_user
@@ -33,19 +39,24 @@ class Api::V1::AdminController < ApplicationController
 		end
 	end
 
+	# Funciones privadas de clase.
 	private
+	# Regresa los parámetros para hacer el update de un usuario.
 	def update_user_params
 		params.require(:user).permit(:email, :name, :father_last_name, :mother_last_name, :dependence_id, :user_role_id)
 	end
 
+	# Regresa el usuario que usará la clase para cualquier método según su id requerido.
 	def get_user
 		@user ||= User.find_by(id: params[:id])
 	end
 
+	# Regresa el mensaje de error cuando un usuario no es administrador.
 	def render_unauthorized_user
 		render json: { error: "No está autorizado para accesar." }, status: 401
 	end
 
+	# Regresa el mensaje de error cuando un registro no se encuentra.
 	def render_record_not_found
 		render json: { error: "El ticket no se encontró." }, status: 404
 	end
