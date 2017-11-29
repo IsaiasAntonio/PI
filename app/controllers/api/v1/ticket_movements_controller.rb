e7e643d8a3b4942398ba458aa40cab446d982472
Paco Chacon <pacochacon94@gmail.com>
2017-11-29 00:22:07 -0600

    Delete token requirement for ticket movements

--- app/controllers/api/v1/ticket_movements_controller.rb
+++ app/controllers/api/v1/ticket_movements_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TicketMovementsController < ApplicationController
 	rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
-	before_action :authenticate_user!
+	before_action :authenticate_user!, :except => [:index, :show]
 
 	def index
 		@ticket_movements = TicketMovement.where(ticket_id: params[:ticket_id])
